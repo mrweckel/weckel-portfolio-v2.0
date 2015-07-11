8dea22b1710cd04e2aada2edac0e32f1bb236941
mrweckel <mrweckel@gmail.com>
2015-07-11 18:28:05 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    minimist = require('minimist'),
+    autoprefixer = require('gulp-autoprefixer'),
+    mochaPhantomjs = require('gulp-mocha-phantomjs');
