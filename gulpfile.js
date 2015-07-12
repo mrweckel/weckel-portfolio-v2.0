f9e8a0eabe83c0b67cb7f47991f93f19a521e254
mrweckel <mrweckel@gmail.com>
2015-07-11 20:34:25 -0400

    Adds gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
 var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify'),
     minimist = require('minimist'),
     autoprefixer = require('gulp-autoprefixer'),
     mochaPhantomjs = require('gulp-mocha-phantomjs');
+
+
+
+gulp.task('browser-sync', ['dist-build'],function(){
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  })
+});
+
+gulp.task('dist-build', function(){
+  console.log('...building dist folder');
+  return gulp.src('./dev/**')
+  .pipe(gulp.dest('./dist'));
+});
+
+
