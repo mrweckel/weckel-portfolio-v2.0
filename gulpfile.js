592247edabe884b0bbe7a4cfa055425c21a0d9bf
mrweckel <mrweckel@gmail.com>
2015-07-12 22:31:27 -0400

    Adds run sequence plugin

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,21 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     minimist = require('minimist'),
     autoprefixer = require('gulp-autoprefixer'),
-    mochaPhantomjs = require('gulp-mocha-phantomjs');
+    mochaPhantomjs = require('gulp-mocha-phantomjs'),
+    runSequence = require('run-sequence');
 
 
 
-gulp.task('browser-sync', ['dist-build'],function(){
-  browserSync.init({
-    server: {
-      baseDir: './staging'
-    }
-  })
-});
+
 
 //====COMPILING====//
 
-//Compile all files
+//Compile html
 gulp.task('compile', function(){
-  console.log('...building dist folder');
+  console.log('...building staging folder');
   return gulp.src('./dev/**')
   .pipe()
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest('./staging'));
 });
 
 //Compile sass
@@ -43,24 +38,39 @@ gulp.task('prefix-styles', function(){
   console.log('...adding vendor prefixes');
   gulp.src('./dev/stylesheets/**')
   .pipe(autoprefixer({
-    browsers: browsers,
-    cascade: false
+    browsers: 'last 2 versions'
   }))
   .pipe(gulp.dest('./dist/stylesheets/'));
 });
 
-//
+//JS hinting
 gulp.task('jshint', function(){
   gulp.src('dev/javascripts/**')
   .pipe(jshint())
   .pipe(jshint.reporter('jshint-stylish'))
-  .pipe(gulp.dest('staging'))
+  .pipe(gulp.dest('staging'));
 });
 
+//Copying JS
+gulp.task('copy-js', function(){
+  gulp.src('dev/javascripts/**')
+  .pipe(gulp.dest('staging'));
+});
+
+//js minification
+gulp.task('uglify', function() {
+  console.log('..minifying that js');
+  gulp.src('./staging/js/**')
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'))
+})
+
+//----BUILD----//
+
 //clean staging file
-gulp.task('clean-stage', function() {
+gulp.task('clean-staging', function() {
   del([
-    'dist/**'
+    'staging/**'
     ]);
   console.log('..cleaning staging folder')
 });
@@ -73,13 +83,68 @@ gulp.task('clean-dist', function() {
   console.log('..cleaning dist folder')
 });
 
-//js minificatoin
-gulp.task('uglify', function() {
-  console.log('..minifying that js');
-  gulp.src('./staging/js/**')
-  .pipe(uglify())
-  .pipe(gulp.dest('dist/js'))
-})
+//render styles
+gulp.task('render-styles', function(callback) {
+  runSequence(
+    'compile-sass',
+    'prefix-styles',
+    callback
+   );
+});
+
+//render scripts
+gulp.task('render-scripts', function(callback) {
+  runSequence(
+    'jshint',
+    'copy-js',
+    callback
+  );
+});
+
+//start server
+gulp.task('browser-sync', function(){
+  browserSync.init({
+    server: {
+      baseDir: './staging'
+    }
+  })
+});
+
+//build staging
+gulp.task('build-staging', function(callback) {
+  runSequence(
+    'clean-staging',
+    'clean-dist',
+    'render-styles',
+    'render-scripts',
+     callback
+  );
+});
+
+gulp.task('init', function(callback) {
+  runSequence(
+    'build-staging',
+    'browser-sync',
+    callback
+  );
+});
+
+// ---- WATCHING ---- //
+gulp.task('watch', function() {
+  gulp.watch('./dev/scss/*.scss', ['render-styles',browserSync.reload]);
+  gulp.watch('./dev/js/*.js', ['render-scripts',browserSync.reload]);
+});
+
+// ---- INITIALIZATION ---- //
+
+// ---- SET DEFAULT ---- //
+gulp.task('default', function(callback) {
+  runSequence(
+    'init',
+    callback
+    )
+});
+
 
 
 
