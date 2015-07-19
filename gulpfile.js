af576e76bd5bb9357daa31f2cbf9cdaf542cdbe5
mrweckel <mrweckel@gmail.com>
2015-07-19 15:27:08 -0400

    Adds watch functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp'),
-    browserSync = require('browser-sync').create(),
+    browserSync = require('browser-sync'),
     del = require('del'),
     jshint = require('gulp-jshint'),
     sass = require('gulp-sass'),
@@ -9,38 +9,50 @@ var gulp = require('gulp'),
     minimist = require('minimist'),
     autoprefixer = require('gulp-autoprefixer'),
     mochaPhantomjs = require('gulp-mocha-phantomjs'),
-    runSequence = require('run-sequence');
-
-
-
+    runSequence = require('run-sequence'),
+    minifyCss = require('gulp-minify-css'),
+    glob = require("glob");
 
 
 //====COMPILING====//
 
 //Compile html
-gulp.task('compile', function(){
-  console.log('...building staging folder');
-  return gulp.src('./dev/**')
-  .pipe()
-  .pipe(gulp.dest('./staging'));
+gulp.task('compile-html', function(){
+  console.log('...getting you that sweet sweet HTML');
+  return gulp.src('./dev/*.html')
+  .pipe(gulp.dest('staging/'));
+});
+
+gulp.task('copy-html', function(){
+  console.log('...copying over html');
+  return gulp.src('staging/*.html')
+  .pipe(gulp.dest('dist/'));
 });
 
 //Compile sass
 gulp.task('compile-sass', function(){
   console.log('...compiling sass');
-  gulp.src('./staging/stylesheets/**')
+  gulp.src('dev/stylesheets/**')
   .pipe(sass())
-  .pipe(gulp.dest('./dist/stylesheets/'));
+  .pipe(gulp.dest('staging/stylesheets/'));
 });
 
 //Add vendor prefixes
 gulp.task('prefix-styles', function(){
   console.log('...adding vendor prefixes');
-  gulp.src('./dev/stylesheets/**')
+  gulp.src('staging/stylesheets/*.css')
   .pipe(autoprefixer({
     browsers: 'last 2 versions'
   }))
-  .pipe(gulp.dest('./dist/stylesheets/'));
+  .pipe(gulp.dest('staging/stylesheets/'));
+});
+
+//Minify CSS
+gulp.task('minify-css', function() {
+  console.log('...minifying css');
+  return gulp.src('staging/stylesheets/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'));
 });
 
 //JS hinting
@@ -48,13 +60,13 @@ gulp.task('jshint', function(){
   gulp.src('dev/javascripts/**')
   .pipe(jshint())
   .pipe(jshint.reporter('jshint-stylish'))
-  .pipe(gulp.dest('staging'));
+  .pipe(gulp.dest('./staging/js/'));
 });
 
 //Copying JS
 gulp.task('copy-js', function(){
   gulp.src('dev/javascripts/**')
-  .pipe(gulp.dest('staging'));
+  .pipe(gulp.dest('staging/js/'));
 });
 
 //js minification
@@ -67,7 +79,6 @@ gulp.task('uglify', function() {
 
 //----BUILD----//
 
-//clean staging file
 gulp.task('clean-staging', function() {
   del([
     'staging/**'
@@ -86,6 +97,7 @@ gulp.task('clean-dist', function() {
 //render styles
 gulp.task('render-styles', function(callback) {
   runSequence(
+    'compile-html',
     'compile-sass',
     'prefix-styles',
     callback
@@ -105,37 +117,46 @@ gulp.task('render-scripts', function(callback) {
 gulp.task('browser-sync', function(){
   browserSync.init({
     server: {
-      baseDir: './staging'
+      baseDir: './staging/'
     }
   })
 });
 
 //build staging
-gulp.task('build-staging', function(callback) {
+gulp.task('build-staging', ['clean-staging'], function() {
   runSequence(
-    'clean-staging',
-    'clean-dist',
     'render-styles',
-    'render-scripts',
-     callback
+    'render-scripts'
   );
 });
 
-gulp.task('init', function(callback) {
+//build dist
+gulp.task('build-dist', ['clean-dist'], function(cb) {
+  runSequence(
+    'copy-html',
+    'minify-css',
+    'uglify',
+     cb
+  );
+});
+
+// ---- INITIALIZATION ---- //
+gulp.task('init', function(cb) {
   runSequence(
     'build-staging',
+    // 'build-dist',
     'browser-sync',
-    callback
+    'watch',
+    cb
   );
 });
 
 // ---- WATCHING ---- //
 gulp.task('watch', function() {
-  gulp.watch('./dev/scss/*.scss', ['render-styles',browserSync.reload]);
-  gulp.watch('./dev/js/*.js', ['render-scripts',browserSync.reload]);
+  gulp.watch('dev/stylesheets/*.scss', ['render-styles',browserSync.reload]);
+  gulp.watch('dev/js/*.js', ['render-scripts',browserSync.reload]);
+  gulp.watch('dev/*.html', ['compile-html',browserSync.reload]);
 });
-
-// ---- INITIALIZATION ---- //
 
 // ---- SET DEFAULT ---- //
 gulp.task('default', function(callback) {
