84bb7f3a284b35c44e4a2d93b400f939478d5f24
mrweckel <mrweckel@gmail.com>
2015-11-29 11:57:25 -0500

    Refactor

--- dev/javascripts/app.js
+++ dev/javascripts/app.js
@@ -5,12 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var doc = document,
         win = window,
         body = document.body,
-        stage = doc.querySelector('#stage'),
         logoPath = doc.querySelector('#logo_svg'),
-        button = doc.querySelector('#button'),
         logoSize = logo.getBoundingClientRect(),
-        xPos = 0,
-        yPos = 0
+        homepage = doc.querySelector('#page-1');
 
         //margins on each side of logo
     var horizontalMargin = win.innerWidth,
@@ -21,37 +18,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     win.addEventListener('resize', function() {
+      //homepage logo
         logoSize = logo.getBoundingClientRect();
         horizontalMargin = logoSize.left;
-        var diff = horizontalMargin - 323;
-
     });
 
-    doc.addEventListener('mousemove', function(e) {
-       horizontalPercent = Math.floor(e.clientX/(horizontalMargin*.5) * 100);
+    homepage.addEventListener('mousemove', function(e) {
+       horizontalPercent = Math.floor(e.clientX/(horizontalMargin* 0.5) * 100);
        verticalPercent = Math.floor(e.clientY/verticalMargin) * 100;
-
-       console.log(e.clientX, e.clientY);
-       console.log(verticalMargin);
-       console.log(horizontalPercent);
        logoPath.style.fill = "hsl(" + (horizontalPercent+250) + ",100%,52%)";
     }, false);
 
 
     doc.addEventListener('mousedown', function(e) {
-
-      console.log("hello");
     }, false);
 
     doc.addEventListener('mouseup', function(e) {
-        if (e.srcElement === button){
-            button.classList.remove("button_down");
-            stage.className = "page_active-2";
-        }
     }, false);
 
     //mobile
-    stage.addEventListener('touchstart',handleStart, false);
+    homepage.addEventListener('touchstart',handleStart, false);
     stage.addEventListener('touchend', handleEnd, false);
     stage.addEventListener('touchmove', handleMove, false);
 
@@ -61,12 +47,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function handleEnd (e) {
     	e.preventDefault();
-    	console.log(e);
-        logo.className = 'ground_zero';
     }
 
     function handleMove (e) {
-    	addMorph(e.touches[0].clientX, e.touches[0].clientY);
+    console.log(e.touches[0].clientX);
+      horizontalPercent = Math.floor(e.touches[0].clientX/(horizontalMargin*0.5) * 100);
+      verticalPercent = Math.floor(e.clientY/verticalMargin) * 100;
+      logoPath.style.fill = "hsl(" + (horizontalPercent+250) + ",100%,52%)";
     }
 
 }, false);
