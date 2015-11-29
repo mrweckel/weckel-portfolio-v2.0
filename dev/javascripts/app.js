6f4ee033487e8b5257303a87fe9a7febcaec70a2
mrweckel <mrweckel@gmail.com>
2015-11-28 19:26:17 -0500

    Implements svgMorph

--- dev/javascripts/app.js
+++ dev/javascripts/app.js
@@ -18,34 +18,34 @@ document.addEventListener('DOMContentLoaded', function() {
         logoSize = logo.getBoundingClientRect();
     });
 
-    doc.addEventListener('mousemove', function(e) {
-       addMorph(e.clientX, e.clientY);
-    }, false);
+    // doc.addEventListener('mousemove', function(e) {
+    //    addMorph(e.clientX, e.clientY);
+    // }, false);
 
 
-    doc.addEventListener('mousedown', function(e) {
-        console.log(e.srcElement.id);
-        if(e.srcElement === button) {
-            button.classList.add("button_down");
-            }
-        if(e.srcElement.id === ('page-1')){
-            stage.classList.add('flash_animation');
-            setTimeout(function() {
-                stage.classList.remove('flash_animation');
-            }, 250);
-        }
+    // doc.addEventListener('mousedown', function(e) {
+    //     console.log(e.srcElement.id);
+    //     if(e.srcElement === button) {
+    //         button.classList.add("button_down");
+    //         }
+    //     if(e.srcElement.id === ('page-1')){
+    //         stage.classList.add('flash_animation');
+    //         setTimeout(function() {
+    //             stage.classList.remove('flash_animation');
+    //         }, 250);
+    //     }
 
-        if(e.srcElement.id === 'menu_icon'){
-            stage.classList.toggle('menu_active')
-        }
-    }, false);
+    //     if(e.srcElement.id === 'menu_icon'){
+    //         stage.classList.toggle('menu_active')
+    //     }
+    // }, false);
 
-    doc.addEventListener('mouseup', function(e) {
-        if (e.srcElement === button){
-            button.classList.remove("button_down");
-            stage.className = "page_active-2";
-        }
-    }, false);
+    // doc.addEventListener('mouseup', function(e) {
+    //     if (e.srcElement === button){
+    //         button.classList.remove("button_down");
+    //         stage.className = "page_active-2";
+    //     }
+    // }, false);
 
 
     stage.addEventListener('touchstart',handleStart, false);
@@ -66,16 +66,18 @@ document.addEventListener('DOMContentLoaded', function() {
     	addMorph(e.touches[0].clientX, e.touches[0].clientY);
     }
 
-    function addMorph (xPos, yPos) {
-    	if (xPos <= logoSize.right &&
-            xPos >= logoSize.left &&
-            yPos >= logoSize.top &&
-            yPos <= logoSize.bottom) {
-    				xPos = 0;
-            logo.className = 'morph-circle';
-      } else {
-          logo.className = 'ground_zero_delay';
-      }
-    }
+    // function addMorph (xPos, yPos) {
+    // 	if (xPos <= logoSize.right &&
+    //         xPos >= logoSize.left &&
+    //         yPos >= logoSize.top &&
+    //         yPos <= logoSize.bottom) {
+    // 				xPos = 0;
+    //         logo.className = 'morph-circle';
+    //   } else {
+    //       logo.className = 'ground_zero_delay';
+    //   }
+    // }
+
+    TweenLite.to("#logo_svg", 1, {morphSVG:"#circle"});
 
 }, false);
