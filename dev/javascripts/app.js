0b7882c4d8d6fbcda55eed6c7226fa359a93c4ee
mrweckel <mrweckel@gmail.com>
2015-11-29 21:32:00 -0500

    Adds morphing functionality

--- dev/javascripts/app.js
+++ dev/javascripts/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var doc = document,
         win = window,
         body = document.body,
+        stage = doc.querySelector('#stage'),
         logoPath = doc.querySelector('#logo_svg'),
         logoSize = logo.getBoundingClientRect(),
         homepage = doc.querySelector('#page-1'),
@@ -16,6 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var verticalMargin = logoSize.top,
         verticalPercent = 0;
+
+    var buildingBackground = document.querySelector("#building-1"),
+        homeIconPath = MorphSVGPlugin.convertToPath(document.querySelector("#building-2")),
+        filmPath = "M91.5,161.9H32.2v-55.7h59.3V161.9z M95.8,112.3h77.7v29H95.8 V112.3z M127.5,153.4H95.8v-8h31.7V153.4z M178,111.7l9-5v38.7l-9-3.9V111.7z M32,54.6c0,0,19.2-28.7,41.2-10.5 s18.2,38.6,18.2,38.6H32V54.6z M32,87.2h59.5v14.7H32V87.2z M116.5,107.9h-15.8V97.4h15.8V107.9z M28,101.9l-15,2.5V85.2l15,2 V101.9z";
 
     win.addEventListener('resize', function() {
       //homepage logo
@@ -38,14 +43,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     doc.addEventListener('mouseup', function(e) {
         console.log(e.target);
+
         if(e.target.id === "nav_arrow"){
             console.log("hello");
             scrollToElement(250, win.innerHeight);
+            stage.className = "page_active-2";
         }
 
         if(e.target.id === "home_icon"){
-            console.log("hello");
-          // TweenMax.to('#home_icon',1, {morphSVG:"M5.1,4.6l188.5,188.5 M5.1,193.2L193.7,4.6"});
+            buildingBackground.style.opacity = "0";
+            TweenMax.to(homeIconPath,1, {morphSVG: {shape: filmPath, shapeIndex:1}});
+            homeIconPath.style.f
         }
     }, false);
 
