5adc02a9de48a0a4839da20d55fa7d9b93ea5b00
mrweckel <mrweckel@gmail.com>
2016-05-29 13:46:14 -0400

    Improper use of self - must revisit

--- dev/javascripts/app.js
+++ dev/javascripts/app.js
@@ -1,3 +1,4 @@
+/*jslint browser:true */
 'use strict';
 
 var Portfolio = {};
@@ -12,19 +13,41 @@ Portfolio.Settings = function() {
     this.screenHeight = this.win.innerHeight;
     this.screenWidth = this.win.innerWidth;
     this.originalScreenPosition = 0;
-    this.mouseDown = 0;
     this.mobileDevice = false;
-}
+};
 
 Portfolio.Settings.prototype = {
 
-    isMobile: function(){
+    checkMobile: function() {
 
+        var isMobile =
+            {
+                Android: function() {
+                    return navigator.userAgent.match(/Android/i);
+                },
+                BlackBerry: function() {
+                    return navigator.userAgent.match(/BlackBerry/i);
+                },
+                iOS: function() {
+                    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+                },
+                Opera: function() {
+                    return navigator.userAgent.match(/Opera Mini/i);
+                },
+                Windows: function() {
+                    return navigator.userAgent.match(/IEMobile/i);
+                },
+                any: function() {
+                    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+                }
+            };
+        return isMobile.any();
     }
-}
+};
 
 //TIMELAPSE OBJECT
 Portfolio.Timelapse = function(doc) {
+
     this.bgImgElements = ['timelapse_reel-1','timelapse_reel-2'];
     this.numOfFrames = 50;
     this.increments = 100/this.numOfFrames;
@@ -32,7 +55,7 @@ Portfolio.Timelapse = function(doc) {
     this.ele = doc.querySelector('#timelapse_inner');
     this.overlay = doc.querySelector('#timelapse_overlay');
     this.container = doc.querySelector('#timelapse_container');
-}
+};
 
 Portfolio.Timelapse.prototype = {
 
@@ -44,14 +67,15 @@ Portfolio.Timelapse.prototype = {
             this.container.classList.remove('background-switch');
         }
     }
-}
+};
 
 //MENU OBJECT
-Portfolio.Menu = function(){
+Portfolio.Menu = function() {
+
     this.closeButton  = document.getElementById('close_button_path').getAttribute('d');
-    this.menuIcon     = document.getElementById('menu_icon_path');
-    this.menuIconPath = this.menuIcon.getAttribute('d');
-}
+    this.icon     = document.getElementById('menu_icon_path');
+    this.iconPath = this.icon.getAttribute('d');
+};
 
 //VIEW
 Portfolio.View = function(doc) {
@@ -59,18 +83,22 @@ Portfolio.View = function(doc) {
     this.stage = doc.querySelector('#stage');
     this.homepage = doc.querySelector('#page-1');
     this.logo = doc.getElementById('logo_svg');
-}
+    this.hotspotContainer =  doc.getElementById('scrub_hotspot_container');
+    this.hotspot = doc.getElementById('scrub_hotspot');
+
+};
 
 
 //CONTROLLER
 Portfolio.Controller = function() {
+
     self = this;
-}
+};
 
 Portfolio.Controller.prototype = {
 
     getBgUrl: function(el) {
-        console.log('hello');
+
         var bg = window.getComputedStyle(el,null).backgroundImage;
         return bg.replace(/url\(['"]?(.*?)['"]?\)/i, "$1");
     },
@@ -91,12 +119,12 @@ Portfolio.Controller.prototype = {
             }, 6000);
 
         arr.forEach(function(bgImg, index) {
+
             var image = document.createElement('img');
             image.src = self.getBgUrl(document.getElementById(bgImg));
             image.onload = function() {
                 if (index === lastElement) {
                     imgsLoaded = true;
-                    console.log("imgsLoaded = " + imgsLoaded);
                     if (timePassed === true) {
                         return cb;
                     }
@@ -115,6 +143,7 @@ Portfolio.Controller.prototype = {
     },
 
     handleMouseUp: function(e) {
+
         console.log("You tapped " + e.target.id, self);
 
         self.onMouseUp({target: e.target, id: e.target.id});
@@ -139,7 +168,7 @@ Portfolio.Controller.prototype = {
             },15);
     },
 
-    handleMouseMove: function(e){
+    handleMouseMove: function(e) {
 
         self.horizontalMove({x: e.clientX});
     },
@@ -160,21 +189,52 @@ Portfolio.Controller.prototype = {
 
         var percent = (xPos/screenWidth)/factor;
         ele.style.webkitTransform = "scale(" + (1 + percent) + ") translateZ(250px)";
+    },
+
+    handleMove: function(e) {
+        self.touchMove({x: e.touches[0].clientX, y: e.touches[0].clientY});
+    },
+
+    handleStart: function(e) {
+
+    },
+
+    handleEnd: function(e) {
+
+        self.touchEnd({event: e});
+    },
+
+    scrubHotspotMove: function(el, parent, xPos, yPos, screenWidth) {
+
+        parent.className = 'inactive';
+
+        var startingX = screenWidth/2;
+
+        if(xPos < startingX){
+          el.style.transform = 'translate3d(' + -(startingX - xPos) + 'px,0px, 0)';
+        } else {
+          el.style.transform = 'translate3d(' + (xPos - startingX) + 'px,0px, 0)';
+        }
+    },
+
+    returnHotspot: function(parentEl) {
+
+        parentEl.className = 'active';
     }
-}
+};
 
 
 
-document.addEventListener('DOMContentLoaded', loadPortfolio, false);
+document.addEventListener('DOMContentLoaded', initPortfolio, false);
 
-function loadPortfolio(){
+function initPortfolio() {
 
     //Create objects
-    var Settings   = new Portfolio.Settings;
+    var Settings   = new Portfolio.Settings();
     var Timelapse  = new Portfolio.Timelapse(Settings.doc);
-    var Menu       = new Portfolio.Menu;
+    var Menu       = new Portfolio.Menu();
     var View       = new Portfolio.View(Settings.doc);
-    var Controller = new Portfolio.Controller;
+    var Controller = new Portfolio.Controller();
 
 
      //Add Loading Animation
@@ -201,13 +261,13 @@ function loadPortfolio(){
             case 'menu_icon_container':
                 stage.classList.add('menu_active');
                 stage.classList.remove('title_animate');
-                TweenMax.to(menuIcon,1, {morphSVG: {shape: closeButton, shapeIndex:5}});
+                TweenMax.to(Menu.icon,1, {morphSVG: {shape: Menu.closeButton, shapeIndex:5}});
                 break;
 
             case 'close_button_container':
                 stage.classList.remove('menu_active');
                 stage.classList.add('title_animate');
-                TweenMax.to(menuIcon,1, {morphSVG: {shape: menuIconPath, shapeIndex:5}});
+                TweenMax.to(Menu.icon,1, {morphSVG: {shape: Menu.iconPath, shapeIndex:5}});
                 break;
 
             case 'home_icon':
@@ -240,81 +300,36 @@ function loadPortfolio(){
         Controller.timelapseMove(Timelapse.ele, args.x, Settings.screenWidth, Timelapse.numOfFrames, Timelapse.increments);
 
         Controller.timelapseScale(Timelapse.container, args.x, Settings.screenWidth, 20);
-    }
-
-    //mobile
-
-    //mobile specific variables
-    var hotspotContainer = doc.getElementById('scrub_hotspot_container'),
-        hotspot = doc.getElementById('scrub_hotspot');
-
-
-    //Check for mobile
-    var isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        },
-        any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
     };
 
-    mobileDevice = isMobile.any() ?  stage.classList.add('mobile_device') : false;
 
-    homepage.addEventListener('touchstart',handleStart, false);
-    homepage.addEventListener('touchend', handleEnd, false);
-    homepage.addEventListener('touchmove', handleMove, false);
+    //Mobile
+    Settings.mobileDevice = Settings.checkMobile() ?  true : false;
 
-    function handleStart (e) {
-        // e.preventDefault();
+    if(Settings.mobileDevice){
+
+        View.stage.classList.add('mobile_device');
+
+        View.homepage.addEventListener('touchstart', Controller.handleStart, false);
+        View.homepage.addEventListener('touchend'  , Controller.handleEnd  , false);
+        View.homepage.addEventListener('touchmove' , Controller.handleMove , false);
     }
 
-    function handleEnd (e) {
-        // e.preventDefault();
+    Controller.touchMove = function(args) {
 
-        returnHotspot(hotspotContainer);
-    }
+        var horMove = args.x;
+        var vertMove = args.y;
 
-    function handleMove (e) {
+        Controller.changeLogoColor(View.logo, args.x, Settings.screenWidth);
 
-        var horMove = e.touches[0].clientX;
-        var vertMove = e.touches[0].clientY;
+        Controller.timelapseMove(Timelapse.ele, args.x, Settings.screenWidth, Timelapse.numOfFrames, Timelapse.increments);
 
-        changeLogoColor(LOGO,horMove);
+        Controller.timelapseScale(Timelapse.container, args.x, Settings.screenWidth, 20);
 
-        timelapseMove(TIMELAPSE, horMove);
-        timelapseScale(TIMELAPSE_CONTAINER, horMove, 20);
+        Controller.scrubHotspotMove(View.hotspot, View.hotspotContainer, horMove, vertMove, Settings.screenWidth);
+    };
 
-        scrubHotspotMove(hotspot, hotspotContainer, horMove, vertMove);
-
-    }
-
-    function scrubHotspotMove(el, parent, xPos, yPos){
-
-        parent.className = 'inactive';
-
-        var startingX = screenWidth/2;
-
-        if(xPos < startingX){
-          el.style.transform = 'translate3d(' + -(startingX - xPos) + 'px,0px, 0)';
-        } else {
-          el.style.transform = 'translate3d(' + (xPos - startingX) + 'px,0px, 0)';
-        }
-    }
-
-    function returnHotspot(parentEl){
-        parentEl.className = 'active';
-    }
+    Controller.touchEnd = function(args) {
+        Controller.returnHotspot((View.hotspotContainer));
+    };
 }
